a60cae79f08fc17577cd829fb1a7d7a1d0f26de3
Taha Mustafa <tahamustafa0019@gmail.com>
2025-06-28 11:06:15 +0530

    Time: 106 ms (35.76%), Space: 71.4 MB (20.23%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
@@ -2,31 +2,24 @@ class NumMatrix {
     private int[][] prefix;
 
     public NumMatrix(int[][] matrix) {
-        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
-            return;
-        }
-
-        int m = matrix.length, n = matrix[0].length;
-        prefix = new int[m][n];
+        int m = matrix.length;
+        int n = matrix[0].length;
+        prefix = new int[m + 1][n + 1];
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                prefix[i][j] = matrix[i][j];
-
-                if (i > 0) prefix[i][j] += prefix[i - 1][j];
-                if (j > 0) prefix[i][j] += prefix[i][j - 1];
-                if (i > 0 && j > 0) prefix[i][j] -= prefix[i - 1][j - 1];
+                prefix[i + 1][j + 1] = matrix[i][j]
+                                     + prefix[i][j + 1]
+                                     + prefix[i + 1][j]
+                                     - prefix[i][j];
             }
         }
     }
 
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        int total = prefix[row2][col2];
-
-        if (row1 > 0) total -= prefix[row1 - 1][col2];
-        if (col1 > 0) total -= prefix[row2][col1 - 1];
-        if (row1 > 0 && col1 > 0) total += prefix[row1 - 1][col1 - 1];
-
-        return total;
+        return prefix[row2 + 1][col2 + 1]
+             - prefix[row1][col2 + 1]
+             - prefix[row2 + 1][col1]
+             + prefix[row1][col1];
     }
 }
